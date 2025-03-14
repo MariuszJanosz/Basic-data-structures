f451cde5e22ffcad9443f269128283fc3b9d39f1
Mariusz Janosz <>
2025-03-14 23:30:42 +0100

    Implementation of a queue data structure as a linked list.

--- main.c
+++ main.c
@@ -5,6 +5,7 @@
 #include "doubly_linked_list.h"
 #include "circular_linked_list.h"
 #include "stack.h"
+#include "queue.h"
 
 int main(int argc, char** argv)
 {
@@ -73,7 +74,21 @@ int main(int argc, char** argv)
 
 		StackPrint(&stack);
 
-		printf("Size of a stack:\n%llu\n", StackGetSize(&stack));
+		printf("Size of a stack: %llu\n", StackGetSize(&stack));
+	}
+	printf("<Queue>\n");
+	{
+		Queue queue;
+		QueueInitialize(&queue);
+
+		QueueEnqueue(&queue, 1);
+		QueueEnqueue(&queue, 2);
+		QueueEnqueue(&queue, 3);
+		QueueEnqueue(&queue, 4);
+		QueueEnqueue(&queue, 5);
+		QueueEnqueue(&queue, 6);
+
+		QueuePrint(&queue);
 	}
 	return 0;
 }
