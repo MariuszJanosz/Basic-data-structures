858157df13d61d9dfb288153909a4dbf5833f81a
Mariusz Janosz <>
2025-03-16 21:08:42 +0100

    Started coding binary tree data structure.

--- src/main.c
+++ src/main.c
@@ -7,34 +7,35 @@
 #include "stack/stack.h"
 #include "queue/queue.h"
 #include "hash_table/hash_table.h"
+#include "binary_trees/binary_tree/binary_tree.h"
 
 int main(int argc, char** argv)
 {
 	printf("<Linked list>\n");
 	{
 		LinkedList* head = NULL;
-
+	
 		head = LinkedListInsertAtBeginning(head, 1);
 		head = LinkedListInsertAtBeginning(head, 2);
 		head = LinkedListInsertAtBeginning(head, 3);
 		head = LinkedListInsertAtBeginning(head, 4);
 		head = LinkedListInsertAtEnd(head, 7);
 		head = LinkedListInsertAtPosition(head, 10, 60);
-
+	
 		LinkedList* tmp = LinkedListFindTail(head);
 		head = LinkedListRemoveElement(head, tmp);
 		head = LinkedListRemoveHead(head);
 		head = LinkedListRemovePosition(head, 3);
-
+	
 		head = LinkedListRevers(head);
-
+	
 		LinkedListPrint(head);
 		LinkedListEmpty(head);
 	}
 	printf("<Doubly linked list>\n");
 	{
 		DoublyLinkedList* head = NULL;
-
+	
 		head = DoublyLinkedListInsertAtBeginning(head, 1);
 		head = DoublyLinkedListInsertAtBeginning(head, 2);
 		head = DoublyLinkedListInsertAtBeginning(head, 3);
@@ -42,27 +43,27 @@ int main(int argc, char** argv)
 		head = DoublyLinkedListInsertAtBeginning(head, 5);
 		head = DoublyLinkedListInsertAtPosition(head, 10, 3);
 		head = DoublyLinkedListInsertAtEnd(head, 7);
-
+	
 		head = DoublyLinkedListRemoveHead(head);
 		head = DoublyLinkedListRemoveElement(head, head->next);
 		head = DoublyLinkedListRemovePosition(head, 1);
 		head = DoublyLinkedListRemoveTail(head);
-
+	
 		head = DoublyLinkedListRevers(head);
-
+	
 		DoublyLinkedListPrint(head);
 		DoublyLinkedListEmpty(head);
 	}
 	printf("<Circular linked list>\n");
 	{
 		CircularLinkedList* head = NULL;
-
+	
 		head = CircularLinkedListInsertAtBeginning(head, 1);
 		head = CircularLinkedListInsertAtBeginning(head, 2);
 		head = CircularLinkedListInsertAtBeginning(head, 3);
 		head = CircularLinkedListInsertAtPosition(head, 5, 1);
 		head = CircularLinkedListInsertAtEnd(head, 10);
-
+	
 		CircularLinkedListPrint(head);
 		CircularLinkedListEmpty(head);
 	}
@@ -70,12 +71,12 @@ int main(int argc, char** argv)
 	{
 		Stack stack;
 		StackInitialize(&stack);
-
+	
 		StackPush(&stack, 1);
 		StackPush(&stack, 2);
 		StackPush(&stack, 3);
 		StackPush(&stack, 4);
-
+	
 		StackPrint(&stack);
 		printf("Size of a stack: %llu\n", StackGetSize(&stack));
 		StackEmpty(&stack);
@@ -84,14 +85,14 @@ int main(int argc, char** argv)
 	{
 		Queue queue;
 		QueueInitialize(&queue);
-
+	
 		QueueEnqueue(&queue, 1);
 		QueueEnqueue(&queue, 2);
 		QueueEnqueue(&queue, 3);
 		QueueEnqueue(&queue, 4);
 		QueueEnqueue(&queue, 5);
 		QueueEnqueue(&queue, 6);
-
+	
 		QueuePrint(&queue);
 		QueueEmpty(&queue);
 	}
@@ -136,6 +137,19 @@ int main(int argc, char** argv)
 		HashTablePrint(&hashtable);
 
 		HashTableFree(&hashtable);
+	}
+	printf("<Binary tree>\n");
+	{
+		BinaryTree* root = NULL;
+
+		root = BinaryTreeInsert(root, 1);
+		root = BinaryTreeInsert(root, 2);
+		root = BinaryTreeInsert(root, 3);
+		root = BinaryTreeInsert(root, 4);
+		root = BinaryTreeInsert(root, 5);
+		root = BinaryTreeInsert(root, 6);
+
+		BinaryTreePrint(root);
 	}
 	return 0;
 }
